606b60e39d666aded0b010ae5fc3620dda6f1d47
DNNgesa <pro.drakengesa@gmail.com>
2020-04-03 18:46:26 +0300

    add dropdown select box

--- js/script.js
+++ js/script.js
@@ -1,70 +1,76 @@
 function validate(){
-  var d = parseInt(dd);
-  var m = parseInt(mm);
-  var y = parseInt(yy);
-  var c = parseInt(cc);
+  var DD = parseInt(document.forms["myForm"]["bd"].value);
+  var MM = parseInt(document.forms["myForm"]["mnth"].value);
+  var YY = parseInt(document.forms["myForm"]["yr"].value);
+  var CC = parseInt(document.forms["myForm"]["yr"].value);
 
-  var math = ( ( (c/4) -2*c-1) + ((5*y/4) ) + ((26*(m+1)/10)) + d) % 7;
-  var exact = Math.round(math);
+  console.log(`${DD},${MM},${YY},${CC}`);
+  
 
-  if(exact === 0 && op1.checked === true){
+  var day = ( ( (CC/4)-2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD) % 7;
+  var dayName = Math.round(day);
+
+  console.log(`${dayName}`);
+  
+
+  if(dayName === 0 && op1.checked === true){
       swal("Your name is Kwasi");
   }
-  else if(exact === 0 && op2.checked === true){
+  else if(dayName === 0 && op2.checked === true){
     swal("Your name is Akosua");
   }
-  if(exact === 1 && op1.checked === true){
+  if(dayName === 1 && op1.checked === true){
       swal("Your name is Kwadwo");
   }
-  else if(exact === 0 && op2.checked === true){
+  else if(dayName === 1 && op2.checked === true){
     swal("Your name is Adwoa");
   }
-  if(exact === 2 && op1.checked === true){
+  if(dayName === 2 && op1.checked === true){
       swal("Your name is Kwabena");
   }
-  else if(exact === 0 && op2.checked === true){
+  else if(dayName === 2 && op2.checked === true){
     swal("Your name is Abenaa");
   }
-  if(exact === 3 && op1.checked === true){
+  if(dayName === 3 && op1.checked === true){
       swal("Your name is Kwaku");
   }
-  else if(exact === 0 && op2.checked === true){
+  else if(dayName === 3 && op2.checked === true){
     swal("Your name is Akua");
   }
-  if(exact === 4 && op1.checked === true){
+  if(dayName === 4 && op1.checked === true){
       swal("Your name is Yaw");
   }
-  else if(exact === 0 && op2.checked === true){
+  else if(dayName === 4 && op2.checked === true){
     swal("Your name is Yaa");
   }
-  if(exact === 5 && op1.checked === true){
+  if(dayName === 5 && op1.checked === true){
       swal("Your name is Kofi");
   }
-  else if(exact === 0 && op2.checked === true){
+  else if(dayName === 5 && op2.checked === true){
     swal("Your name is Afua");
   }
-  if(exact === 6 && op1.checked === true){
+  if(dayName === 6 && op1.checked === true){
       swal("Your name is Kwame");
   }
-  else if(exact === 0 && op2.checked === true){
+  else if(dayName === 6 && op2.checked === true){
     swal("Your name is Ama");
   }
 
-  if(dd === '' || mm === || yy === '' || cc == ''){
+  if(DD === '' || MM === '' || YY === '' || CC == ''){
     swal ({
       title: "Error", 
       text: "You haven't filled all fields!",
       icon: "warning",
       });
   }
-  else if(dd <=0 || dd>31){
+  else if(DD <=0 || DD>31){
     swal({
       title: "Error",
       text: "Invalid Date",
       icon: "warning"
       });
   }
-  else if(mm <=0 || mm>12){
+  else if(MM <=0 || MM>12){
     swal({
       title: "Error",
       text: "Invalid Date",
